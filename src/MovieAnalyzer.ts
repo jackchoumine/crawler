5b2bb6b37a95cbd26acefda05c0d4a5a9ec3e033
jack <jackzhoumine@gmail.com>
2021-10-24 20:22:51 +0800

    将分析器弄成单例模式

--- src/MovieAnalyzer.ts
+++ src/MovieAnalyzer.ts
@@ -2,7 +2,7 @@
  * @Description : 提取电影信息
  * @Date        : 2021-10-24 19:17:34 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-24 20:05:05 +0800
+ * @LastEditTime: 2021-10-24 20:20:51 +0800
  * @LastEditors : JackChou
  */
 import * as cheerio from 'cheerio'
@@ -12,11 +12,19 @@ export interface IAnalyzer {
   analyze: (html: string, filePath: string) => Movie[]
 }
 export default class MovieAnalyzer implements IAnalyzer {
+  private constructor() {}
+  private static instance: MovieAnalyzer
+  static getInstance() {
+    if (!MovieAnalyzer.instance) {
+      MovieAnalyzer.instance = new MovieAnalyzer()
+    }
+    return MovieAnalyzer.instance
+  }
+
   analyze(htmlCode: string, dataFilepath: string) {
     const movies = this.getMovieInfo(htmlCode)
     return this.generateMovieList(movies, dataFilepath)
   }
-
   private getMovieInfo(htmlCode: string): Movie[] {
     const $ = cheerio.load(htmlCode)
     const playing = $('#nowplaying')
