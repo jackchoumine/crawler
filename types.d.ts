c9a94ccb3abb4000acf59b8f6e47ecc8b8138329
jack <jackzhoumine@gmail.com>
2021-10-25 01:55:08 +0800

    类型融合和类型扩展

--- types.d.ts
+++ types.d.ts
@@ -1,8 +1,8 @@
 /*
  * @Description :
- * @Date        : 2021-10-25 01:42:39 +0800
+ * @Date        : 2021-10-25 01:53:09 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-25 01:46:59 +0800
+ * @LastEditTime: 2021-10-25 01:53:09 +0800
  * @LastEditors : JackChou
  */
 declare namespace Express {
