05040fb185d6acdd223fb8753640792e57b13983
jack <jackzhoumine@gmail.com>
2021-10-25 03:10:14 +0800

    添加获取电影的接口

--- src/route/index.ts
+++ src/route/index.ts
@@ -2,10 +2,13 @@
  * @Description :
  * @Date        : 2021-10-25 00:30:58 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-25 02:21:58 +0800
+ * @LastEditTime: 2021-10-25 03:07:48 +0800
  * @LastEditors : JackChou
  */
-import { Router, Request } from 'express'
+import { Router, Request, Response } from 'express'
+import fs from 'fs'
+import path from 'path'
+import { Movie } from '../types'
 // type RequestWithBody = Request & {
 //   body: {
 //     [key: string]: unknown
@@ -50,7 +53,29 @@ router.post('/login', (req: RequestWithBody, res) => {
   }
 })
 
-router.get('/movies', (req, res) => {
-  res.send({ data: 'hello world' })
+router.get('/movies/:id?', (req: Request<{ id: number }>, res: Response) => {
+  const filePath = path.resolve(__dirname, '../../data/playing.json')
+  const { id } = req.params
+  console.log(req.query)
+  if (fs.existsSync(filePath)) {
+    const json = fs.readFileSync(filePath, 'utf-8')
+    const data = JSON.parse(json)
+    const queryKeys = Object.keys(req.query)
+    if (queryKeys.length) {
+      // @ts-ignore
+      const filteredData = filterData(queryKeys, data, req.query)
+      res.send(filteredData)
+    } else {
+      res.send(id ? data[id] : data)
+    }
+  } else {
+    res.send([])
+  }
 })
+
+function filterData<K extends keyof Movie>(keys: K[], data: Movie[], query: Record<string, unknown>): Movie[] {
+  return data.filter(item => {
+    return keys.some(key => item[key] == query[key])
+  })
+}
 export default router
