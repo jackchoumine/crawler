799780f9a5caabcd565fffb406fd192c9549123e
jack <jackzhoumine@gmail.com>
2021-10-24 18:03:47 +0800

    获取豆瓣正在上映的电影

--- src/crawler.ts
+++ src/crawler.ts
@@ -2,16 +2,28 @@
  * @Description : 爬虫类
  * @Date        : 2021-10-24 17:11:43 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-24 17:33:37 +0800
+ * @LastEditTime: 2021-10-24 18:02:27 +0800
  * @LastEditors : JackChou
  */
 import * as superagent from 'superagent'
+import * as cheerio from 'cheerio'
 class Crawler {
   private url = 'https://movie.douban.com/cinema/nowplaying/chengdu/'
-  private rawHtml = ''
+  private playingMovies: unknown[] = []
+  getMovieInfo(htmlCode: string) {
+    const $ = cheerio.load(htmlCode)
+    const playing = $('#nowplaying')
+    const list = playing.find('.lists')
+    list.children().each((index, element) => {
+      const cheerioObj = $(element)
+      const poster = cheerioObj.find('.poster').find('img').attr('src')
+      const movie = cheerioObj.data()
+      this.playingMovies.push({ poster: poster, ...movie })
+    })
+  }
   async getRawHtml() {
     const result = await superagent.get(this.url)
-    this.rawHtml = result.text
+    this.getMovieInfo(result.text)
   }
   constructor() {
     this.getRawHtml()
