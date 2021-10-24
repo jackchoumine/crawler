0b32008ee9c3f29910db2e06d33ac7fd1ff7fbef
jack <jackzhoumine@gmail.com>
2021-10-24 17:15:15 +0800

    ts+ts-node可用性测试

---
+++ src/crawler.ts
@@ -0,0 +1,14 @@
+/*
+ * @Description : 爬虫类
+ * @Date        : 2021-10-24 17:11:43 +0800
+ * @Author      : JackChou
+ * @LastEditTime: 2021-10-24 17:14:02 +0800
+ * @LastEditors : JackChou
+ */
+class Crawler {
+  private url = ''
+  constructor() {
+    console.log(22)
+  }
+}
+const crawler = new Crawler()
