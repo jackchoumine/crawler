1c78587e4ff1e7b6c108820176b9d329519294ea
jack <jackzhoumine@gmail.com>
2021-10-24 17:34:49 +0800

    获取html页面

--- src/crawler.ts
+++ src/crawler.ts
@@ -2,13 +2,19 @@
  * @Description : 爬虫类
  * @Date        : 2021-10-24 17:11:43 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-24 17:14:02 +0800
+ * @LastEditTime: 2021-10-24 17:33:37 +0800
  * @LastEditors : JackChou
  */
+import * as superagent from 'superagent'
 class Crawler {
-  private url = ''
+  private url = 'https://movie.douban.com/cinema/nowplaying/chengdu/'
+  private rawHtml = ''
+  async getRawHtml() {
+    const result = await superagent.get(this.url)
+    this.rawHtml = result.text
+  }
   constructor() {
-    console.log(22)
+    this.getRawHtml()
   }
 }
 const crawler = new Crawler()
