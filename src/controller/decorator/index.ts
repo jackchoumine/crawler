cfe77b9391b67f0c9cb96a0b734f5de18c5b75e2
jack <jackzhoumine@gmail.com>
2021-10-26 22:46:25 +0800

    feat: 添加工具函数

--- src/controller/decorator/index.ts
+++ src/controller/decorator/index.ts
@@ -2,20 +2,33 @@
  * @Description :
  * @Date        : 2021-10-26 21:36:17 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-26 21:57:19 +0800
+ * @LastEditTime: 2021-10-26 22:45:27 +0800
  * @LastEditors : JackChou
  */
 import { Router } from 'express'
-
+import { isFunction } from '../../utils'
+// FIXME 编译后无法识别路径别名
+// import { isFunction } from '@utils/index'
 export const router = Router()
 
 export function controller(constructor: any) {
-  for (const key in constructor.prototype) {
-    const path = Reflect.getMetadata('path', constructor.prototype, key)
-    if (path) {
-      router.get(path, constructor.prototype[key])
+  const prototype = constructor.prototype
+
+  Object.keys(prototype).forEach(key => {
+    const path = Reflect.getMetadata('path', prototype, key)
+    const handler = prototype[key]
+    // if (path && method && handler) {
+    if (path && isFunction(handler)) {
+      router.get(path, handler)
     }
-  }
+  })
+
+  // for (const key in prototype) {
+  //   const path = Reflect.getMetadata('path', prototype, key)
+  //   if (path) {
+  //     router.get(path, prototype[key])
+  //   }
+  // }
 }
 
 export function get(path: string) {
