34343d298a074f7b2e80ba36c43f713e14b440a3
jack <jackzhoumine@gmail.com>
2021-10-26 23:52:03 +0800

    添加类型

--- src/route/types.d.ts
+++ src/route/types.d.ts
@@ -2,11 +2,16 @@
  * @Description :
  * @Date        : 2021-10-25 01:53:09 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-25 01:53:09 +0800
+ * @LastEditTime: 2021-10-26 23:37:12 +0800
  * @LastEditors : JackChou
  */
 declare namespace Express {
   interface Request {
     name: string
   }
+}
+
+// = Record<PropertyKey,unknown>
+declare interface BodyRequest<T extends Record<PropertyKey, unknown>> extends Request {
+  body: T
 }
