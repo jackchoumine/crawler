aa65ac56250b55979eb8d3ad3701435805deeb5c
jack <jackzhoumine@gmail.com>
2021-10-26 23:51:58 +0800

    使用装饰器模式优化代码

--- src/controller/decorator/index.ts
+++ src/controller/decorator/index.ts
@@ -2,7 +2,7 @@
  * @Description :
  * @Date        : 2021-10-26 21:36:17 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-26 22:59:27 +0800
+ * @LastEditTime: 2021-10-26 23:34:11 +0800
  * @LastEditors : JackChou
  */
 import { Router } from 'express'
@@ -11,16 +11,24 @@ import { isFunction } from '../../utils'
 // import { isFunction } from '@utils/index'
 export const router = Router()
 
+enum Method {
+  get = 'get',
+  post = 'post',
+}
+
 export function controller(constructor: any) {
   const prototype = constructor.prototype
 
   Object.keys(prototype).forEach(key => {
     const path = Reflect.getMetadata('path', prototype, key)
+    const method: Method = Reflect.getMetadata('method', prototype, key)
     console.log(path)
+    console.log(method)
     const handler = prototype[key]
-    // if (path && method && handler) {
-    if (path && isFunction(handler)) {
-      router.get(path, handler)
+    if (path && method && isFunction(handler)) {
+      // NOTE 将 method 声明为 Method 类型，不再报错
+      // WHY
+      router[method](path, handler)
     }
   })
 
@@ -35,5 +43,24 @@ export function controller(constructor: any) {
 export function get(path: string) {
   return function (prototype: any, methodName: string, descriptor: PropertyDescriptor) {
     Reflect.defineMetadata('path', path, prototype, methodName)
+    Reflect.defineMetadata('method', 'get', prototype, methodName)
+  }
+}
+
+export const post = getRequestDecorator(Method.post)
+// NOTE 装饰器模式改善代码
+// export function post(path: string) {
+//   return function (prototype: any, methodName: string, descriptor: PropertyDescriptor) {
+//     Reflect.defineMetadata('path', path, prototype, methodName)
+//     Reflect.defineMetadata('method', 'post', prototype, methodName)
+//   }
+// }
+
+function getRequestDecorator(method: Method) {
+  return function (path: string) {
+    return function (prototype: any, methodName: string, descriptor: PropertyDescriptor) {
+      Reflect.defineMetadata('path', path, prototype, methodName)
+      Reflect.defineMetadata('method', method, prototype, methodName)
+    }
   }
 }
