e1754a309e749b44fb3c547c2610d737a9b38771
jack <jackzhoumine@gmail.com>
2021-10-29 23:19:52 +0800

    feat: 提交评论接口

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,10 +2,11 @@
  * @Description : 处理器
  * @Date        : 2021-10-26 21:08:55 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-29 21:13:43 +0800
+ * @LastEditTime: 2021-10-29 22:56:27 +0800
  * @LastEditors : JackChou
  */
 
 export * from './LoginController'
 export * from './MovieController'
 export * from './ProductController'
+export * from './ReviewController'
