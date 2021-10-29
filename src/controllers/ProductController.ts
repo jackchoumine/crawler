21ca1cfc828254e9c5820b3f1eef15d2ce6c1979
jack <jackzhoumine@gmail.com>
2021-10-29 21:43:52 +0800

    feat： 添加查询条件

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -2,7 +2,7 @@
  * @Description :
  * @Date        : 2021-10-29 21:08:03 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-29 21:33:11 +0800
+ * @LastEditTime: 2021-10-29 21:42:56 +0800
  * @LastEditors : JackChou
  */
 import { Request, Response } from 'express'
@@ -16,7 +16,16 @@ import { get, controller } from '../decorators'
 class ProductController {
   @get('/products')
   async getProducts(req: Request, res: Response): Promise<any> {
-    const products = await prisma.product.findMany()
+    const products = await prisma.product.findMany({
+      where: {
+        // name: { equals: 'Shoe' },
+        // name: { contains: 'mac' },
+        // price: {
+        //   gt: 100,
+        //   lt: 1400,
+        // },
+      },
+    })
     res.json(products)
   }
 }
