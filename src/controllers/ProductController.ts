a559763d0df7015c2635c980a6516aa53af28ca2
jack <jackzhoumine@gmail.com>
2021-10-29 22:18:50 +0800

    优化控制器

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -2,7 +2,7 @@
  * @Description :
  * @Date        : 2021-10-29 21:08:03 +0800
  * @Author      : JackChou
- * @LastEditTime: 2021-10-29 21:56:58 +0800
+ * @LastEditTime: 2021-10-29 22:13:26 +0800
  * @LastEditors : JackChou
  */
 import { Request, Response } from 'express'
@@ -12,24 +12,44 @@ const prisma = new PrismaClient()
 
 import { post, get, controller } from '../decorators'
 
-@controller()
+@controller('/products')
 class ProductController {
-  @get('/products')
+  @get('/:id?')
   async getProducts(req: Request, res: Response): Promise<any> {
-    const products = await prisma.product.findMany({
-      where: {
-        // name: { equals: 'Shoe' },
-        // name: { contains: 'mac' },
-        price: {
-          gt: 100,
-          lt: 1400,
+    const { id } = req.params
+    console.log('id')
+    console.log(id)
+    if (id) {
+      try {
+        //@ts-ignore
+        const product = await prisma.product.findOne({
+          where: {
+            id: { equals: id },
+          },
+        })
+        console.log(product)
+        res.json(product)
+      } catch (error) {
+        //  @ts-ignore
+        console.log(error.message)
+        res.json({ error })
+      }
+    } else {
+      const products = await prisma.product.findMany({
+        where: {
+          // name: { equals: 'Shoe' },
+          // name: { contains: 'mac' },
+          price: {
+            gt: 100,
+            lt: 1400,
+          },
         },
-      },
-    })
-    res.json(products)
+      })
+      res.json(products)
+    }
   }
 
-  @post('/products')
+  @post()
   async createProduct(req: Request, res: Response) {
     const { body } = req
     const product = await prisma.product.create({
